bf1374884a0750190228e9ae988d22499cb5447d
Soobin Kwon <soobkwon@uw.edu>
2017-01-10 16:24:39 -0800

    Add the completed Module 6 Exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,15 +2,20 @@
 
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
-
-
+AddThree <- function(x) {
+  return(3 + x)
+}
+  
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
 # The function should return the total length in meters
-
+ImperialToMetric <- function(num.of.feet, num.of.inch) {
+  return((num.of.feet * 0.3048) + (num.of.inch * 0.0254))
+}
 
 # Create a variable `height.in.meters` by passing your height in imperial to the
 # `ImperialToMetric` function
+height.in.meters <- ImperialToMetric(5, 3)
